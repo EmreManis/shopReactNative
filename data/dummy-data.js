a3143430e3b73e7c52aa9a367c9717f3827e108e
Emre Manış <emremn07@gmail.com>
2021-12-07 13:54:28 +0300

    added data and model

---
+++ data/dummy-data.js
@@ -0,0 +1,98 @@
+import Product from "../models/product";
+
+export const ProductItems = [
+  new Product(
+    "Shoes",
+    "https://en.wikipedia.org/wiki/Shoe#/media/File:Puma_shoes.jpg",
+    49.99,
+    `Made in USA or Imported
+        Rubber sole
+        Lightweight mesh upper with 3-color digital print delivers complete breathability
+        Durable leather overlays for stability & that locks in your midfoot
+        EVA sockliner provides soft, step-in comfort
+        Charged Cushioning midsole uses compression molded foam for ultimate responsiveness & durability
+        Solid rubber outsole covers high impact zones for greater durability with less weight`
+  ),
+
+  new Product(
+    "Bed Sheet Set",
+    "https://en.wikipedia.org/wiki/Bed_sheet#/media/File:Prześcieradło.jpg",
+    142.99,
+    `100% Polyester Microfiber
+    Imported
+    Queen bed sheet set includes (1) 90 x 102 inch flat sheet, (1) 60 x 80 x 14 inch fitted sheet, and (2) 20 x 30 inch Standard pillowcases
+    All-around elastic on 14 inch fitted sheet allows for snug, secure fit of most mattress sizes up to a 16 inch mattress depth
+    Super soft and durable polyester microfiber (85 GSM) fabric offers all-se`
+  ),
+
+  new Product(
+    "Colored Pencils",
+    "https://en.wikipedia.org/wiki/Colored_pencil#/media/File:Stabilo_color_pencils.jpg",
+    12.99,
+    `CRAYOLA COLORED PENCILS: This set features 50 rich, vibrant colors that highlight the details of every coloring book and art project
+    AT HOME CRAFTS and INDOOR ACTIVITIES: Keep spirits high with creative art supplies; Simple and fun crafts for kids are a great way to stay thoughtfully engaged and entertained
+    GREAT FOR KIDS & ADULTS: The color selection and quality of these Crayola Colored Pencils make them great art supplies for adults and children`
+  ),
+
+  new Product(
+    "Watch",
+    "https://en.wikipedia.org/wiki/Electric_watch#/media/File:Hamilton_505.JPG",
+    142.99,
+    `Get ready to crush your wellness goals with body readings right on your wrist.
+    Better Sleep Starts Here: Wake up feeling refreshed and recharged with advanced sleep tracking; When you go to bed, your Galaxy Watch4 sleep tracker starts monitoring your sleep and SpO2 levels continuously.
+    Be Smart About Your Heart: Take care of your heart with accurate ECG monitoring and keep an eye on possible atrial fibrillation, a common form of irregular heart rhythm; Share personalized readings with your doctor using the Samsung Health Monitor app on your compatible Galaxy phone.`
+  ),
+
+  new Product(
+    "Baby Shirts",
+    "https://en.wikipedia.org/wiki/Infant_bodysuit#/media/File:Baby_in_an_infant_bodysuit.jpg",
+    42.99,
+    `Top 1 and 2: 100% Cotton; Top 3: 98% Cotton, 2% Polyester
+    Imported
+    Button closure
+    Machine Wash
+    Pocket details`
+  ),
+  new Product(
+    "Writing Desk",
+    "https://en.wikipedia.org/wiki/Writing_desk#/media/File:Writing_Desk_(Italy),_ca._1902_(CH_18446625-2).jpg",
+    427.99,
+    `Easy-to-assemble, modern design constructed of engineered wood with sleek black paper laminate
+        Small and Convenient design for those tight places
+        Great for Homes, Dorms, Bedrooms and more
+        Sturdy Engineered wood`
+  ),
+  new Product(
+    "Earbuds",
+    "https://en.wikipedia.org/wiki/Headphones#/media/File:InEarMonitors.jpg",
+    99.99,
+    `SOUND THAT ROCKS: The AKG-tuned 12mm speaker and enhanced bass tone play everything in studio-quality sound.
+      NEW STYLE, NEW SOUND: Equal parts earbud and ear bling, Galaxy Buds Live come in 4 colors you’ll want to rock all day.
+      TURN DOWN DISTRACTIONS: Turn on Active noise cancellation to block out background noise on an airplane, train or bus.`
+  ),
+  new Product(
+    "White 5200 Blender",
+    "https://en.wikipedia.org/wiki/Blender#/media/File:ElectricBlender.jpg",
+    495.99,
+    `Variable Speed Control: Easily adjust speed to achieve a variety of textures. The dial can be rotated at any point during the blend, so you’re in complete control
+        Large Batches: The size and shape of the self-cleaning 64-ounce container is ideal for blending medium to large batches. Design Feature : Radial cooling fan and thermal protection system
+        Hot Soup: The blades in the Vitamix container reach speeds fast enough to create friction heat, bringing cold ingredients to steaming hot in about six minutes`
+  ),
+  new Product(
+    "ASUS VivoBook",
+    "https://en.wikipedia.org/wiki/Laptop#/media/File:Acer_Chromebook_11_(24394834161).jpg",
+    796.95,
+    `15.6 inch Full HD OLED NanoEdge bezel display with stunning 85% screen-to-body ratio
+        Latest 11th generation Intel Core i5-1135G7 Processor 2.4 GHz (8M Cache, up to 4.2 GHz) and Intel Iris Xᵉ Graphics
+        8GB DDR4 on board + 4GB DDR4 SO-DIMM RAM and 512GB PCIe NVMe M.2 SSD and Windows 10 Home`
+  ),
+  new Product(
+    "Gloves for Men Women",
+    "https://en.wikipedia.org/wiki/Glove#/media/File:Isotoner_touch_gloves_BB_E86_jeh.jpg",
+    46.99,
+    `95% Acrylic, 5% Spandex
+      UPGRADED TOUCH SCREEN: 50% more conductive yarn added on touchscreen fingertips than others, you can better touch screen of your smartphone, iPad, tablet as well as your car while driving without taking your hands out in freezing weather.
+      Spot wash by hand in luke warm water with mild detergent. Hang to dry. Do not use hot water. Do not put in dryer.
+      ☛HIGH SENSITIVE TOUCHSCREEN: Featuring high sensitive conductive material on thumbs, forefingers and middle fingers, you can wear the gloves to touch screen of your smartphone, tablet, iPhone or other touch screen devices`
+  )
+];
---
+++ models/product.js
@@ -0,0 +1,15 @@
+class Product {
+    constructor (
+        title,
+        image,
+        price,
+        detail
+    )
+    {
+        this.title = title;
+        this.image = image;
+        this.price = price;
+        this.detail = detail;
+    }
+}
+export default Product;
